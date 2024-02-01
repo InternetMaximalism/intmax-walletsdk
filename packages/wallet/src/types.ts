bfaa34390f3bde4220609d5bf8cd0578ec9af07d
aoi kimura <ecdfhacker@proton.me>
2024-02-01 23:27:18 +0900

    feat(wallet): define abstract message type

--- packages/wallet/src/types.ts
+++ packages/wallet/src/types.ts
@@ -1,3 +1,18 @@
-export type Namespace = "eip155";
+export type Namespace = "webmax" | "eip155";
 
 export type ClientMode = "popup" | "iframe";
+
+export type AbstractRequest = {
+	id: number;
+	namespace: Namespace;
+	method: string;
+	params: unknown[];
+};
+
+export type AbstractResponse = {
+	id: number;
+	namespace: Namespace;
+	handling: "keep" | "close";
+	result?: unknown;
+	error?: string;
+};
