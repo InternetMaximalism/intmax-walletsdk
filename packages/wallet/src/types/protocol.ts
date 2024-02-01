cabe72617e6181e6be57675525b1d822f77e38db
aoi kimura <ecdfhacker@proton.me>
2024-02-02 01:23:50 +0900

    feat(wallet): update ns

--- packages/wallet/src/types/protocol.ts
+++ packages/wallet/src/types/protocol.ts
@@ -1,6 +1,12 @@
 import type { AddEthereumChainParameter, TransactionRequest, WatchAssetParams } from "viem";
 
-export type Namespace = "webmax" | "eip155";
+export type Namespaces = {
+	EIP155: "eip155";
+	EIP155_CHAIN: `eip155:${number}`;
+	WEBMAX: "webmax";
+};
+
+export type Namespace = Namespaces[keyof Namespaces];
 
 export type EthereumAddress = `0x${string}`;
 export type Hex = `0x${string}`;
@@ -8,7 +14,6 @@ export type Hash = `0x${string}`;
 
 export type WebmaxHandshakeResult = {
 	supportedNamespaces: Namespace[];
-	chains: Record<Namespace, string[]>;
 };
 
 export type WebmaxMessageSchema = [
@@ -25,17 +30,18 @@ export type WebmaxMessageSchema = [
 	 */
 	{
 		method: "webmax_forbidden";
-		params?: [method: string];
+		params?: [napespace: Namespace, method: string];
 		result?: undefined;
 	},
 ];
 
-export type EIP155MessageSchema = [
+export type EIP1193MessageSchema = [
 	/**
 	 * @description Requests that the user provides an Ethereum address to be identified by. This request also implies permission to access other methods.
 	 * @link https://eips.ethereum.org/EIPS/eip-1102
 	 */
 	{
+		namespace: Namespaces["EIP155_CHAIN"];
 		method: "eth_requestAccounts";
 		params?: undefined;
 		result: EthereumAddress[];
@@ -45,41 +51,49 @@ export type EIP155MessageSchema = [
 	 * @link https://eips.ethereum.org/EIPS/eip-1193
 	 */
 	{
+		namespace: Namespaces["EIP155_CHAIN"];
 		method: "eth_sendTransaction";
 		params: [transaction: TransactionRequest];
 		result: Hash;
 	},
 	{
+		namespace: Namespaces["EIP155_CHAIN"] | Namespaces["EIP155"];
 		method: "eth_sign";
 		params: [address: EthereumAddress, data: Hex];
 		result: Hex;
 	},
 	{
+		namespace: Namespaces["EIP155_CHAIN"];
 		method: "eth_signTransaction";
 		params: [transaction: TransactionRequest];
 		result: Hex;
 	},
 	{
+		namespace: Namespaces["EIP155_CHAIN"];
 		method: "eth_signTypedData_v4";
 		params: [address: EthereumAddress, message: string];
 		result: Hex;
 	},
 	{
+		namespace: Namespaces["EIP155_CHAIN"] | Namespaces["EIP155"];
 		method: "personal_sign";
 		params: [data: Hex, address: EthereumAddress];
 		result: Hex;
 	},
 	{
+		namespace: Namespaces["EIP155"];
 		method: "wallet_addEthereumChain";
 		params: [chainParams: AddEthereumChainParameter];
 		result: null;
 	},
 	{
+		namespace: Namespaces["EIP155"];
 		method: "wallet_switchEthereumChain";
 		params: [chain: { chainId: string }];
 		result: null;
 	},
 	{
+		namespace: Namespaces["EIP155_CHAIN"];
 		method: "wallet_watchAsset";
 		params: [asset: WatchAssetParams];
 		result: null;
