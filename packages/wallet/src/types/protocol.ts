b861e96835c206d9a9560eb98ec3fc5299090d54
aoi kimura <ecdfhacker@proton.me>
2024-02-02 00:09:56 +0900

    feat(wallet): add comments to types

--- packages/wallet/src/types/protocol.ts
+++ packages/wallet/src/types/protocol.ts
@@ -12,19 +12,38 @@ export type WebmaxHandshakeResult = {
 };
 
 export type WebmaxMessageSchema = [
+	/**
+	 * @description Handshake message sent when popup is opened and returns supported namespaces and chains to dApp.
+	 */
 	{
 		method: "webmax_handshake";
 		params?: undefined;
 		result: WebmaxHandshakeResult;
 	},
+	/**
+	 * @description Forbidden message sent when dApp tries to call a method before walletconnect.
+	 */
+	{
+		method: "webmax_forbidden";
+		params?: [method: string];
+		result?: undefined;
+	},
 ];
 
 export type EIP155MessageSchema = [
+	/**
+	 * @description Requests that the user provides an Ethereum address to be identified by. This request also implies permission to access other methods.
+	 * @link https://eips.ethereum.org/EIPS/eip-1102
+	 */
 	{
 		method: "eth_requestAccounts";
 		params?: undefined;
 		result: EthereumAddress[];
 	},
+	/**
+	 * @description Sign and send a new transaction.
+	 * @link https://eips.ethereum.org/EIPS/eip-1193
+	 */
 	{
 		method: "eth_sendTransaction";
 		params: [transaction: TransactionRequest];
