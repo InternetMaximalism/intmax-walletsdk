d56becd521b1e438fd957c1a969d4da75b8b6914
aoi kimura <ecdfhacker@proton.me>
2024-02-05 17:02:40 +0900

    feat(example): add packages

--- examples/wallet/src/app/layout.tsx
+++ examples/wallet/src/app/layout.tsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router-dom";
 
 function GlobalLayout() {
 	return (
-		<>
-			<Outlet />
-		</>
+		<div className="h-[100dvh] max-h-[100dvh] overflow-hidden bg-muted sm:p-4 lg:p-6">
+			<div className="mx-auto max-w-sm sm:border rounded-md bg-background h-full">
+				<Outlet />
+			</div>
+		</div>
 	);
 }
 
