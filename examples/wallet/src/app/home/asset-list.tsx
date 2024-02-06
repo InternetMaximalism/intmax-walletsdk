0c1922fd677673953f7c3cdeeb3bc1b446176204
aoi kimura <ecdfhacker@proton.me>
2024-02-07 01:41:07 +0900

    feat(example):

--- examples/wallet/src/app/home/asset-list.tsx
+++ examples/wallet/src/app/home/asset-list.tsx
@@ -1,16 +1,46 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { getChainLogo } from "@/constants";
 import { useAccount } from "@/hooks/account";
 import { useBalances } from "@/hooks/balance";
+import { useTokenWithChain } from "@/hooks/blockchain";
+import { tokenKey } from "@/lib/blockchain/utils";
+import { formatBalance, formatUsdBalance } from "@/lib/format";
 import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { FC, memo } from "react";
 
-export const AssetList: FC<{ className: string }> = ({ className }) => {
+export const AssetList: FC<{ className?: string }> = memo(({ className }) => {
 	const account = useAccount();
-	const balances = useBalances(account);
+	const balances = useTokenWithChain(useBalances(account));
 
 	return (
-		<ScrollArea className={cn(className)}>
+		<ScrollArea className={className}>
+			<div className={cn("grid gap-0.5", className)}>
+				{balances.map((balance) => (
+					<div
+						key={tokenKey(balance.token)}
+						className="flex gap-2 items-center rounded-md hover:bg-muted focus:bg-muted px-2 py-2 transition-colors"
+					>
+						<div className="flex items-end">
+							<Avatar className="h-4 w-4 -mr-3 z-10 bg-background">
+								<AvatarImage src={getChainLogo(balance.chain)} alt={balance.chain?.name} />
+								<AvatarFallback>{balance.chain?.name?.toUpperCase().slice(0, 2)}</AvatarFallback>
+							</Avatar>
+							<Avatar className="h-10 w-10 bg-background">
+								<AvatarImage src={balance.token.logoURI} alt={balance.token.symbol} />
+								<AvatarFallback>{balance.token.symbol?.toUpperCase().slice(0, 2)}</AvatarFallback>
+							</Avatar>
+						</div>
+						<div className="flex-1">
+							<div className="text-sm font-medium leading-none">{balance.token.name}</div>
+							<div className="text-sm text-muted-foreground">{`${formatBalance(balance)} ${balance.token.symbol}`}</div>
+						</div>
+						<div className="font-medium">{formatUsdBalance(balance)}</div>
+					</div>
+				))}
+			</div>
 			<ScrollBar orientation="vertical" />
 		</ScrollArea>
 	);
-};
+});
