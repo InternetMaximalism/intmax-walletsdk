2d41a73523ec36f35e058ca7abf03a3c3e2e86e6
aoi kimura <ecdfhacker@proton.me>
2024-02-06 17:28:59 +0900

    feat(example):

--- examples/wallet/src/constants.ts
+++ examples/wallet/src/constants.ts
@@ -1,41 +1,73 @@
 import { Blockchain } from "@ankr.com/ankr.js";
-import { arbitrum, arbitrumSepolia, bsc, mainnet, optimism, polygon, scroll } from "viem/chains";
+import {
+	Chain,
+	arbitrum,
+	arbitrumSepolia,
+	avalanche,
+	avalancheFuji,
+	base,
+	bsc,
+	fantom,
+	flare,
+	gnosis,
+	linea,
+	mainnet,
+	optimism,
+	optimismGoerli,
+	polygon,
+	polygonZkEvm,
+	rollux,
+	scroll,
+	syscoin,
+} from "viem/chains";
 import { bscTestnet, goerli, optimismSepolia, polygonMumbai, scrollSepolia, sepolia } from "viem/chains";
 import invariant from "./lib/invariant";
+import { InternalRpcUrl } from "./types";
 
 export const ANKR_API_KEY =
-	process.env.VITE_ANKR_API_KEY ?? "98e9658e77cb8fbb473c21f0129d90c08531fa006a2f82b9b81bf294041ef99b";
+	import.meta.env.VITE_ANKR_API_KEY ?? "98e9658e77cb8fbb473c21f0129d90c08531fa006a2f82b9b81bf294041ef99b";
 invariant(ANKR_API_KEY, "ANKR_API_KEY is required");
 
 export const ANKR_BALANCE_CHAIN_MAP = {
+	[arbitrum.id]: "arbitrum",
+	[avalanche.id]: "avalanche",
+	[avalancheFuji.id]: "avalanche_fuji",
+	[base.id]: "base",
+	[bsc.id]: "bsc",
 	[mainnet.id]: "eth",
 	[goerli.id]: "eth_goerli",
+	[fantom.id]: "fantom",
+	[flare.id]: "flare",
+	[gnosis.id]: "gnosis",
+	[linea.id]: "linea",
+	[optimism.id]: "optimism",
+	[optimismGoerli.id]: "optimism_testnet",
 	[polygon.id]: "polygon",
 	[polygonMumbai.id]: "polygon_mumbai",
-	[arbitrum.id]: "arbitrum",
-	[optimism.id]: "optimism",
+	[polygonZkEvm.id]: "polygon_zkevm",
+	[rollux.id]: "rollux",
 	[scroll.id]: "scroll",
-	[bsc.id]: "bsc",
+	[syscoin.id]: "syscoin",
 } satisfies Record<number, Blockchain>;
 
+export const getAnkrBalanceID = (chian: Chain) => {
+	const id = ANKR_BALANCE_CHAIN_MAP[chian.id as keyof typeof ANKR_BALANCE_CHAIN_MAP];
+	return id ?? null;
+};
+
+//TODO: Add the rest of the chains
 export const ANKR_RPC_CHAIN_MAP = {
-	[mainnet.id]: "eth",
-	[goerli.id]: "eth_goerli",
+	...ANKR_BALANCE_CHAIN_MAP,
 	[sepolia.id]: "eth_sepolia",
-	[polygon.id]: "polygon",
-	[polygonMumbai.id]: "polygon_mumbai",
-	[optimism.id]: "optimism",
 	[optimismSepolia.id]: "optimism_sepolia",
-	[arbitrum.id]: "arbitrum",
 	[arbitrumSepolia.id]: "arbitrum_sepolia",
-	[scroll.id]: "scroll",
 	[scrollSepolia.id]: "scroll_sepolia",
-	[bsc.id]: "bsc",
 	[bscTestnet.id]: "bsc_testnet_chapel",
 };
 
-export const getAnkrRpcUrl = (chainId: number) => {
-	const rpc = ANKR_RPC_CHAIN_MAP[chainId as keyof typeof ANKR_RPC_CHAIN_MAP];
-	if (!rpc) throw new Error(`No Ankr RPC found for chainId: ${chainId}`);
-	return `https://rpc.ankr.com/${rpc}/${ANKR_API_KEY}`;
+export const getAnkrRpcUrl = (chain: Chain): InternalRpcUrl => {
+	const ankrChainId = ANKR_RPC_CHAIN_MAP[chain.id as keyof typeof ANKR_RPC_CHAIN_MAP];
+	if (!ankrChainId) return { url: chain.rpcUrls.default.http[0] };
+	const ankrRpc = `https://rpc.ankr.com/${ankrChainId}/${ANKR_API_KEY}`;
+	return { url: ankrRpc };
 };
--- examples/wallet/src/main.tsx
+++ examples/wallet/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NotFound from "./app/404";
 import HomePage from "./app/home";
 import GlobalLayout from "./app/layout";
@@ -8,6 +10,8 @@ import SettingsPage from "./app/settings";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
 	{
@@ -27,6 +31,8 @@ if (!root) throw new Error("Root element not found");
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
 		<Toaster />
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
