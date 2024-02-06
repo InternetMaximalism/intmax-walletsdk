4d511efa99f55ad71137ac7262fce5f757d31160
aoi kimura <ecdfhacker@proton.me>
2024-02-07 01:44:33 +0900

    feat(example):

--- examples/wallet/src/app/home/index.tsx
+++ examples/wallet/src/app/home/index.tsx
@@ -2,12 +2,14 @@ import { AccountSwitcher } from "./account-switcher";
 import { AssetList } from "./asset-list";
 import { BalanceCard } from "./balance-card";
 import { MainActions } from "./main-actions";
+import { ThemeSwitcher } from "./theme-switcher";
 
 function HomePage() {
 	return (
 		<div className="p-2 pb-0 grid gap-4 grid-rows-[auto,auto,auto,1fr] max-h-full">
-			<header className="flex justify-end">
+			<header className="flex justify-between">
 				<AccountSwitcher />
+				<ThemeSwitcher />
 			</header>
 			<BalanceCard />
 			<MainActions />
