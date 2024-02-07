8ab45fbc4949b9a827a91e4419c3c376aa6999a9
aoi kimura <ecdfhacker@proton.me>
2024-02-07 17:17:11 +0900

    feat(example):

--- examples/wallet/src/app/layout.tsx
+++ examples/wallet/src/app/layout.tsx
@@ -1,10 +1,14 @@
+import { Loading } from "@/components/loading";
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 function GlobalLayout() {
 	return (
 		<div className="h-[100dvh] max-h-[100dvh] overflow-hidden bg-muted sm:p-4 lg:p-6 flex items-center justify-center">
 			<div className="w-full sm:max-w-sm sm:border sm:aspect-[1/2] rounded-md bg-background h-full sm:h-auto">
-				<Outlet />
+				<Suspense fallback={<Loading />}>
+					<Outlet />
+				</Suspense>
 			</div>
 		</div>
 	);
--- examples/wallet/src/components/drawers/index.tsx
+++ examples/wallet/src/components/drawers/index.tsx
@@ -1,6 +1,7 @@
 import { Drawer, DrawerContent } from "@/components/ui/drawer";
 import { useDrawerStore } from "@/stores/drawers";
 import { Suspense, lazy } from "react";
+import { Loading } from "../loading";
 
 const ProfileDrawer = lazy(() => import("./profile-drawer"));
 const TokenDrawer = lazy(() => import("./token-drawer"));
@@ -13,7 +14,7 @@ export const Drawers = () => {
 	return (
 		<Drawer {...restProps}>
 			<DrawerContent className="min-h-48">
-				<Suspense>
+				<Suspense fallback={<Loading />}>
 					{props?.id === "profile" && <ProfileDrawer {...props} {...restProps} />}
 					{props?.id === "token-detail" && <TokenDrawer {...props} {...restProps} />}
 				</Suspense>
