2b66c61c2b90ebc3011dee272aeb635e4724d8d3
aoi kimura <ecdfhacker@proton.me>
2024-02-12 22:22:03 +0900

    feat(example): update example

--- examples/dapp/src/App.tsx
+++ examples/dapp/src/App.tsx
@@ -1,15 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { webmaxDappClient } from "webmax2/dapp";
 
-const webmax = webmaxDappClient({
-	url: import.meta.env.VITE_WALLET_URL as string,
-	name: "DEMO Wallet",
-});
+const DEFAULT_WALLET_URL = import.meta.env.VITE_WALLET_URL || "https://webmax2-wallet.vercel.app/";
 
 function App() {
+	const [walletUrl, setWalletUrl] = useState(DEFAULT_WALLET_URL);
+	const [webmax, setWebmax] = useState(webmaxDappClient({ url: walletUrl, name: "DEMO Wallet" }));
+
 	const [accounts, setAccounts] = useState<string[]>([]);
 	const [result, setResult] = useState<string>("");
+
+	const isValidUrl = /https?:\/\/[^\s$.?#].[^\s]*$/.test(walletUrl);
+	const handleUpdateWalletUrl = (url: string) => {
+		if (!isValidUrl) return;
+		setWebmax(webmaxDappClient({ url, name: "DEMO Wallet" }));
+	};
 
 	const handleConnect = async () => {
 		await webmax.connect();
@@ -30,10 +37,36 @@ function App() {
 
 	return (
 		<>
-			<Button onClick={handleConnect}>Connect</Button>
-			<Button onClick={handleSignMessage}>Sign Message</Button>
-			<div>{accounts.join(", ")}</div>
-			<div>{result}</div>
+			<div className="w-full max-w-screen-sm mx-auto px-4 py-12 space-y-8">
+				<div>
+					<div className="text-2xl font-semibold">Webmax Dapp Example</div>
+					<div className="text-muted-foreground">This is a simple example of how to use the webmax dapp client.</div>
+				</div>
+				<div>
+					<div className="font-semibold text-lg">Wallet URL</div>
+					<div className="flex gap-2">
+						<Input placeholder="Etner wallet url" onChange={(e) => setWalletUrl(e.target.value)} value={walletUrl} />
+						<Button disabled={!isValidUrl} onClick={() => handleUpdateWalletUrl(walletUrl)}>
+							Update
+						</Button>
+					</div>
+					<div className="text-muted-foreground">
+						The webmax protocol allows you to connect to a wallet of any URL by specifying the wallet's URL!
+					</div>
+				</div>
+				<div className="gap-2 grid grid-cols-2">
+					<Button onClick={handleConnect}>Connect</Button>
+					<Button onClick={handleSignMessage}>Sign Message</Button>
+					<div className="col-span-2">
+						<div className="font-semibold">Accounts:</div>
+						<div className="break-all text-muted-foreground">{accounts.join(", ")}</div>
+					</div>
+					<div className="col-span-2">
+						<div className="font-semibold">Signature:</div>
+						<div className="break-all text-muted-foreground">{result}</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
