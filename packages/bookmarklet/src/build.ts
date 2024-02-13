540db308794d65d17c1778c2f00f656ab5bec67e
aoi kimura <ecdfhacker@proton.me>
2024-02-13 22:53:33 +0900

    fix

--- packages/bookmarklet/src/build.ts
+++ packages/bookmarklet/src/build.ts
@@ -1,11 +1,3 @@
-/*
-  For `build.ts`, further inspire @honojs/hono with inspire @kaze-style/react.
-  https://github.com/honojs/hono/blob/main/build.ts
-  https://github.com/taishinaritomi/kaze-style/blob/main/scripts/build.ts
-  MIT License
-  Copyright (c) 2024 inaridiy
-*/
-
 import { context } from "esbuild";
 
 const esmBuild = () =>
