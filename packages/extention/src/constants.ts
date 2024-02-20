a8fd0f3d6167182a47f0ac4955f5fd3e01fa50bd
Inaridiy <inainainariinainari@gmail.com>
2024-02-21 02:58:50 +0900

    feat(extension): simple wallet switcher

--- packages/extention/src/constants.ts
+++ packages/extention/src/constants.ts
@@ -1,1 +1,5 @@
-export const DEFAULT_WALLETS = [{ name: "Local Wallet", url: "http://localhost:5173" }];
+export const DEFAULT_WALLETS = [
+	{ name: "Local Wallet", url: "http://localhost:5173" },
+	{ name: "INTMAX", url: "https://wallet.intmax.io" },
+	{ name: "Demo Wallet", url: "https://webmax2-wallet.vercel.app/" },
+];
