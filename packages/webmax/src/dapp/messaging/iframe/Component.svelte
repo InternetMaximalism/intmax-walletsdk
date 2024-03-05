308e103716b2847bc6bba5673c8ff89c044bd9f6
kimuraaoi <ecdfhacker@proton.me>
2024-03-05 19:20:22 +0900

    feat: mobile

--- packages/webmax/src/dapp/messaging/iframe/Component.svelte
+++ packages/webmax/src/dapp/messaging/iframe/Component.svelte
@@ -5,14 +5,16 @@
   export let handleIframeRef: (iframeRef: HTMLIFrameElement) => void;
   export let handleClose: () => void;
 
+  const isMobile = window.innerWidth < 640;
+
   let iframeRef: HTMLIFrameElement;
   let dialogRef: HTMLDialogElement;
 
   let dragging = false;
   let startX = 0;
   let startY = 0;
-  let offsetX = window.innerWidth - 20 - 400;
-  let offsetY = 20;
+  let offsetX = isMobile ? 0 : window.innerWidth - 20 - 400;
+  let offsetY = isMobile ? 0 : 20;
 
   $: if (iframeRef) handleIframeRef(iframeRef);
   $: show ? dialogRef?.showModal() : dialogRef?.close();
@@ -76,19 +78,34 @@
   }
 
   .popup_container[open] {
+    overscroll-behavior: none;
     z-index: 2147483646;
     display: grid;
     position: fixed;
     grid-template-rows: auto 1fr;
-    width: 400px;
-    height: 600px;
     overflow: hidden;
-    border-radius: 0.75rem;
     background-color: white;
     cursor: move;
     filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04))
       drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));
     animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+
+  @media (max-width: 640px) {
+    .popup_container {
+      width: 100vw;
+      max-width: 100vw;
+      height: 100dvh;
+      max-height: 100dvh;
+      border-radius: 0;
+    }
+  }
+  @media (min-width: 640px) {
+    .popup_container {
+      width: 400px;
+      height: 600px;
+      border-radius: 0.75rem;
+    }
   }
 
   .popup_nav {
