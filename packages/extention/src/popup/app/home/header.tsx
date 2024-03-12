1efa69c75f3433c09855175640e497decba59a71
kimuraaoi <ecdfhacker@proton.me>
2024-03-12 23:29:55 +0900

    refactor:

--- packages/extention/src/popup/app/home/header.tsx
+++ packages/extention/src/popup/app/home/header.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 import { WalletSwitcher } from "./wallet-switcher";
 
 export const HomeHeader = () => {
 	return (
 		<header className="border-b p-2 flex justify-between items-center">
 			<WalletSwitcher />
-			<Button size="sm">Settings</Button>
+			<Button size="icon" variant="ghost">
+				<Menu />
+			</Button>
 		</header>
 	);
 };
