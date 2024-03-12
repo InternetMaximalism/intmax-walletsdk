584f41c8608c6fbf4511d2bc0257a2572b93e739
kimuraaoi <ecdfhacker@proton.me>
2024-03-12 13:21:21 +0900

    feat:

--- packages/extention/src/lib/messaging/extensionMessaging.ts
+++ packages/extention/src/lib/messaging/extensionMessaging.ts
@@ -1,4 +1,4 @@
-import { Runtime, browser } from "wxt/browser";
+import { Runtime } from "wxt/browser";
 
 // biome-ignore lint/suspicious/noExplicitAny:
 export const defineExtensionMessaging = <T extends Record<string, any> = Record<string, any>>(messagingId: string) => {
@@ -9,14 +9,14 @@ export const defineExtensionMessaging = <T extends Record<string, any> = Record<
 			tabId?: number,
 		): Promise<ReturnType<T[K]>> {
 			const response = tabId
-				? await browser.tabs.sendMessage(tabId, { messagingId, method, data })
-				: await browser.runtime.sendMessage({ messagingId, method, data });
+				? await chrome.tabs.sendMessage(tabId, { messagingId, method, data })
+				: await chrome.runtime.sendMessage({ messagingId, method, data });
 			if (response.error) throw new Error(response.error);
 			return response.result;
 		},
 		async sendEvent<K extends keyof T>(event: K, data: Parameters<T[K]>[0], tabId?: number): Promise<void> {
-			if (tabId) await browser.tabs.sendMessage(tabId, { messagingId, event, data });
-			else await browser.runtime.sendMessage({ messagingId, event, data });
+			if (tabId) await chrome.tabs.sendMessage(tabId, { messagingId, event, data });
+			else await chrome.runtime.sendMessage({ messagingId, event, data });
 		},
 		async onMessage<K extends keyof T>(
 			method: K,
@@ -24,10 +24,10 @@ export const defineExtensionMessaging = <T extends Record<string, any> = Record<
 				| ReturnType<T[K]>
 				| Promise<ReturnType<T[K]>>,
 		): Promise<void> {
-			browser.runtime.onMessage.addListener(
+			chrome.runtime.onMessage.addListener(
 				(message, sender, sendResponse: (response: { result?: ReturnType<T[K]>; error?: string }) => void) => {
 					if (message.messagingId === messagingId && message.method === method) {
-						Promise.resolve(listener({ sender, data: message.data }))
+						Promise.resolve(listener({ sender: sender as Runtime.MessageSender, data: message.data }))
 							.then((response) => sendResponse({ result: response }))
 							.catch((error) => sendResponse({ error: error.message }));
 						// Keeping the message channel open for asynchronous response
@@ -40,9 +40,9 @@ export const defineExtensionMessaging = <T extends Record<string, any> = Record<
 			event: K,
 			listener: (args: { sender: Runtime.MessageSender; data: Parameters<T[K]>[0] }) => void,
 		): Promise<void> {
-			browser.runtime.onMessage.addListener((message, _sender) => {
+			chrome.runtime.onMessage.addListener((message, _sender) => {
 				if (message.messagingId === messagingId && message.event === event) {
-					listener({ sender: _sender, data: message.data });
+					listener({ sender: _sender as Runtime.MessageSender, data: message.data });
 				}
 			});
 		},
